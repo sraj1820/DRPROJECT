52b32fb0b06a2bb0076dbc11c48aa0df5da66dfb
sraj1820 <sraj1820@gmail.com>
2022-04-06 21:49:46 -0400

    Updated Migrations and Models

--- mazda_app/models.py
+++ mazda_app/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Mazda(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    reg_num = models.CharField(max_length=10)
+
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = 'mazda'
 
 class Users(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = 'user'
 
 class Car(models.Model):
     name = models.CharField(max_length=100)
@@ -19,20 +31,15 @@ class Car(models.Model):
     mileage = models.IntegerField()
     color = models.CharField(max_length=50)
     new = models.BooleanField(default = False)
-
+    company = models.ForeignKey(Mazda, on_delete=models.CASCADE)
     users = models.ManyToManyField(Users)
+
      
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = 'car'
 
-class Mazda(models.Model):
-    name = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    reg_num = models.CharField(max_length=10)
 
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
 
 
