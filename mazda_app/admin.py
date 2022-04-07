03ecc51ffbf61fb4f3c623234838028d3f6f419d
Deo G <66809588+921Mdas@users.noreply.github.com>
2022-04-06 21:55:58 -0400

    correcting bug tinker

--- mazda_app/admin.py
+++ mazda_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Car,Users,Mazda
 
 # Register your models here.
+admin.site.register(Car)
+admin.site.register(Users)
+admin.site.register(Mazda)
+
--- mazda_app/models.py
+++ mazda_app/models.py
@@ -1,15 +1,28 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
+class Mazda(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    reg_num = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = 'mazda'
+
 class Users(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = 'user'
 
 class Car(models.Model):
     name = models.CharField(max_length=100)
@@ -19,20 +32,14 @@ class Car(models.Model):
     mileage = models.IntegerField()
     color = models.CharField(max_length=50)
     new = models.BooleanField(default = False)
-
+    company = models.ForeignKey(Mazda, on_delete=CASCADE)
     users = models.ManyToManyField(Users)
      
     def __str__(self):
         return self.name
+        
+    class Meta:
+        verbose_name_plural = 'car'
 
-class Mazda(models.Model):
-    name = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    reg_num = models.CharField(max_length=10)
-
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
 
 
