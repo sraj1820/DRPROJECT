bbb9bb40fce5c560db6a9025efa9bc640b41b951
sraj1820 <sraj1820@gmail.com>
2022-04-12 20:08:05 -0400

    Authentication features

--- mazda_app/views.py
+++ mazda_app/views.py
@@ -7,6 +7,8 @@ from django.views.generic import DetailView
 from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from mazda_app.models import Car
 from .serializers import carSerializer
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -36,3 +38,18 @@ class CarUpdate(UpdateView):
 def car_detail(request, car_id):
           car = Car.objects.get(id=car_id)
           return render(request,'detail.html',{'car':car})
+
+
+def signup(request):
+     error_message = ''
+     if request.method == 'POST':
+          form = UserCreationForm(request.POST)
+          if form.is_valid():
+               user= form.save()
+               login(request,user)
+               return redirect('/accounts/login/')
+          else:
+               error_message = 'invalid sign up, please try again'
+     form = UserCreationForm()
+     context = {'form': form, 'error_message': error_message}
+     return render(request, 'registration/signup.html', context)
